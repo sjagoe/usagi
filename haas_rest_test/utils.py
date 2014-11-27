af087763ae0cebf37f3daf74bac7cddf2309be12
Simon Jagoe <simon@simonjagoe.com>
2014-11-27 20:51:12 +0000

    Move session factory to utils module

---
+++ haas_rest_test/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Simon Jagoe
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE.txt file for details.
+from __future__ import absolute_import, unicode_literals
+
+from requests.utils import default_user_agent as requests_user_agent
+import requests
+
+import haas_rest_test
+
+
+def haas_rest_test_user_agent():
+    return 'haas-rest-test/{0} {1}'.format(
+        haas_rest_test.__version__, requests_user_agent())
+
+
+def create_session():
+    session = requests.Session()
+    session.headers['User-Agent'] = haas_rest_test_user_agent()
+    return session
--- haas_rest_test/yaml_test_loader.py
+++ haas_rest_test/yaml_test_loader.py
@@ -9,29 +9,22 @@ from __future__ import absolute_import, unicode_literals
 import logging
 
 from jsonschema.exceptions import ValidationError
-from requests.utils import default_user_agent as requests_user_agent
 import jsonschema
-import requests
 import six
 import yaml
 
 from haas.module_import_error import ModuleImportError
 from haas.testing import unittest
 
-import haas_rest_test
 from .exceptions import YamlParseError
 from .schema import SCHEMA
+from .utils import create_session
 
 
 logger = logging.getLogger(__name__)
 
 
 TEST_NAME_ATTRIBUTE = 'haas_rest_test_name'
-
-
-def haas_rest_test_user_agent():
-    return 'haas-rest-test/{0} {1}'.format(
-        haas_rest_test.__version__, requests_user_agent())
 
 
 def _create_yaml_parse_error_test(filename, error):
@@ -58,8 +51,7 @@ def _create_test_method(requests_session, test_spec):
 
 
 def create_test_case_for_group(filename, group):
-    session = requests.Session()
-    session.headers['User-Agent'] = haas_rest_test_user_agent()
+    session = create_session()
 
     class_dict = dict(
         ('test_{0}'.format(index), _create_test_method(session, spec))
