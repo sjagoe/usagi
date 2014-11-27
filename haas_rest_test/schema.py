992f50757bb1e51786f81ed20b1257bd17b2c58c
Simon Jagoe <simon@simonjagoe.com>
2014-11-27 18:32:46 +0000

    Add header to schema module

--- haas_rest_test/schema.py
+++ haas_rest_test/schema.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Simon Jagoe
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE.txt file for details.
+from __future__ import absolute_import, unicode_literals
+
 SCHEMA = {
     '$schema': 'http://json-schema.org/draft-04/schema#',
     'title': 'Haas Rest Test test cases',
