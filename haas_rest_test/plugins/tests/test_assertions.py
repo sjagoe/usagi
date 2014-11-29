214e45076e5c7b15a72af33bfc2cf9e359852102
Simon Jagoe <simon@simonjagoe.com>
2014-11-29 13:41:51 +0000

    Add basic header assertion functionality

--- haas_rest_test/plugins/tests/test_assertions.py
+++ haas_rest_test/plugins/tests/test_assertions.py
@@ -6,12 +6,14 @@
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 from __future__ import absolute_import, unicode_literals
 
+import re
+
 from mock import Mock
 
 from haas.testing import unittest
 
 from haas_rest_test.exceptions import YamlParseError
-from ..assertions import StatusCodeAssertion
+from ..assertions import HeaderAssertion, StatusCodeAssertion
 
 
 class TestStatusCodeAssertion(unittest.TestCase):
@@ -90,3 +92,191 @@ class TestStatusCodeAssertion(unittest.TestCase):
 
         # Then
         case.assertEqual.assert_called_once_with(200, 200)
+
+
+class TestHeaderAssertion(unittest.TestCase):
+
+    def test_no_header(self):
+        # Given
+        spec = {
+            'name': 'header',
+        }
+
+        # When
+        with self.assertRaises(YamlParseError):
+            HeaderAssertion.from_dict(spec)
+
+    def test_conflicting_value_regexp(self):
+        # Given
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+            'value': 'value',
+            'regexp': 'regexp',
+        }
+
+        # When
+        with self.assertRaises(YamlParseError):
+            HeaderAssertion.from_dict(spec)
+
+    def test_no_expected_value(self):
+        # Given
+        response = Mock()
+        response.headers = {
+            'Content-Type': 'application/json'
+        }
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+        }
+
+        # When
+        assertion = HeaderAssertion.from_dict(spec)
+
+        # Then
+        self.assertEqual(assertion.header, spec['header'])
+        self.assertEqual(assertion.expected_value, None)
+        self.assertEqual(assertion.regexp, False)
+
+        # When
+        case = Mock()
+        assertion.run(case, response)
+
+        # Then
+        case.assertIn.assert_called_once_with(spec['header'], response.headers)
+        self.assertFalse(case.assertRegexpMatches.called)
+        self.assertFalse(case.assertEqual.called)
+
+    def test_assert_expected_value(self):
+        # Given
+        content_type = 'application/json'
+        expected_content_type = content_type
+        response = Mock()
+        response.headers = {
+            'Content-Type': content_type,
+        }
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+            'value': expected_content_type,
+        }
+
+        # When
+        assertion = HeaderAssertion.from_dict(spec)
+
+        # Then
+        self.assertEqual(assertion.header, spec['header'])
+        self.assertEqual(assertion.expected_value, expected_content_type)
+        self.assertEqual(assertion.regexp, False)
+
+        # When
+        case = Mock()
+        assertion.run(case, response)
+
+        # Then
+        case.assertIn.assert_called_once_with(spec['header'], response.headers)
+        case.assertEqual.assert_called_once_with(
+            content_type, expected_content_type)
+        self.assertFalse(case.assertRegexpMatches.called)
+
+    def test_assert_expected_value_failure(self):
+        # Given
+        content_type = 'application/json'
+        expected_content_type = 'text/html'
+        response = Mock()
+        response.headers = {
+            'Content-Type': content_type,
+        }
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+            'value': expected_content_type,
+        }
+
+        # When
+        assertion = HeaderAssertion.from_dict(spec)
+
+        # Then
+        self.assertEqual(assertion.header, spec['header'])
+        self.assertEqual(assertion.expected_value, expected_content_type)
+        self.assertEqual(assertion.regexp, False)
+
+        # When
+        case = Mock()
+        case.assertEqual.side_effect = AssertionError
+        with self.assertRaises(AssertionError):
+            assertion.run(case, response)
+
+        # Then
+        case.assertIn.assert_called_once_with(spec['header'], response.headers)
+        case.assertEqual.assert_called_once_with(
+            content_type, expected_content_type)
+        self.assertFalse(case.assertRegexpMatches.called)
+
+    def test_assert_expected_regexp(self):
+        # Given
+        content_type = 'application/github.v3+json'
+        expected_content_type = 'application/.*?json'
+        response = Mock()
+        response.headers = {
+            'Content-Type': content_type,
+        }
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+            'regexp': expected_content_type,
+        }
+
+        # When
+        assertion = HeaderAssertion.from_dict(spec)
+
+        # Then
+        self.assertEqual(assertion.header, spec['header'])
+        self.assertEqual(
+            assertion.expected_value, re.compile(expected_content_type))
+        self.assertEqual(assertion.regexp, True)
+
+        # When
+        case = Mock()
+        assertion.run(case, response)
+
+        # Then
+        case.assertIn.assert_called_once_with(spec['header'], response.headers)
+        case.assertRegexpMatches.assert_called_once_with(
+            assertion.expected_value, content_type)
+        self.assertFalse(case.assertEqual.called)
+
+    def test_assert_expected_regexp_failure(self):
+        # Given
+        content_type = 'text/github.v3+json'
+        expected_content_type = 'application/.*?json'
+        response = Mock()
+        response.headers = {
+            'Content-Type': content_type,
+        }
+        spec = {
+            'name': 'header',
+            'header': 'Content-Type',
+            'regexp': expected_content_type,
+        }
+
+        # When
+        assertion = HeaderAssertion.from_dict(spec)
+
+        # Then
+        self.assertEqual(assertion.header, spec['header'])
+        self.assertEqual(
+            assertion.expected_value, re.compile(expected_content_type))
+        self.assertEqual(assertion.regexp, True)
+
+        # When
+        case = Mock()
+        case.assertRegexpMatches.side_effect = AssertionError
+        with self.assertRaises(AssertionError):
+            assertion.run(case, response)
+
+        # Then
+        case.assertIn.assert_called_once_with(spec['header'], response.headers)
+        case.assertRegexpMatches.assert_called_once_with(
+            assertion.expected_value, content_type)
+        self.assertFalse(case.assertEqual.called)
