1106342d4ec5da44fdaaa31f13b2c5bf062f832d
Simon Jagoe <simon@simonjagoe.com>
2014-11-29 11:22:44 +0000

    Add env tests

--- haas_rest_test/tests/test_config.py
+++ haas_rest_test/tests/test_config.py
@@ -6,12 +6,26 @@
 # of the 3-clause BSD license.  See the LICENSE.txt file for details.
 from __future__ import absolute_import, unicode_literals
 
+from contextlib import contextmanager
+import os
+
 from haas.testing import unittest
 
 from ..config import template_variables
 
 
 class TestTemplateVariables(unittest.TestCase):
+
+    @contextmanager
+    def environment(self, **env):
+        old_env = os.environ
+        new_env = os.environ.copy()
+        new_env.update(env)
+        os.environ = new_env
+        try:
+            yield
+        finally:
+            os.environ = old_env
 
     def test_simple(self):
         # Given
@@ -26,6 +40,42 @@ class TestTemplateVariables(unittest.TestCase):
 
         # When
         templated_vars = template_variables(variables)
+
+        self.assertEqual(templated_vars, expected)
+
+    def test_env(self):
+        # Given
+        variables = {
+            'var1': '/some/path',
+            'var2': {'env': 'ENV_VAR'},
+        }
+        expected = {
+            'var1': '/some/path',
+            'var2': '/another/path',
+        }
+
+        # When
+        with self.environment(ENV_VAR='/another/path'):
+            templated_vars = template_variables(variables)
+
+        self.assertEqual(templated_vars, expected)
+
+    def test_env_in_template(self):
+        # Given
+        variables = {
+            'var1': '/some/path',
+            'var2': {'env': 'ENV_VAR'},
+            'var3': {'template': '{var2}/yet/another/path'},
+        }
+        expected = {
+            'var1': '/some/path',
+            'var2': '/another/path',
+            'var3': '/another/path/yet/another/path'
+        }
+
+        # When
+        with self.environment(ENV_VAR='/another/path'):
+            templated_vars = template_variables(variables)
 
         self.assertEqual(templated_vars, expected)
 
